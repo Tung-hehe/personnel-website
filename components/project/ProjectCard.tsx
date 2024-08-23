6616b5fcccef98e04016767bf1553af1c01f7f3b
Tran Thanh Tung <thanhtung1052001@gmail.com>
2024-08-23 09:19:11 +0700

    Add logic puzzle solver project

--- components/project/ProjectCard.tsx
+++ components/project/ProjectCard.tsx
@@ -8,28 +8,30 @@ import type { Project } from '@/data/projects'
 export function ProjectCard({ title, description, imgSrc, href, builtWith }: Project) {
   return (
     <div className='p-4 md:w-1/2'>
-      <div className='h-full overflow-hidden rounded-md border-2 border-opacity-60 border-gray-500'>
+      <div className='flex h-full flex-col overflow-hidden rounded-md border-2 border-opacity-60 border-gray-500'>
         <Link href={href}>
           <Image
             alt={title}
             title={title}
             src={imgSrc}
             className="object-cover object-center md:h-36 lg:h-48"
-            width={600}
-            height={300}
+            width={544}
+            height={306}
           />
         </Link>
-        <div className="p-6">
-          <h2 className="mb-3 text-2xl font-bold tracking-tight">
-            <Link href={href}>{title}</Link>
-          </h2>
-          <p className="prose mb-3 text-gray-400 text-justify">{description}</p>
-          <div className="flex flex-wrap space-x-1.5 mb-3">
-            <span className="font-semibold text-gray-300">
-              {builtWith.join(', ')}
-            </span>
+        <div className="flex grow flex-col justify-between p-6 flex flex-col">
+          <div>
+            <h2 className="mb-3 text-2xl font-bold tracking-tight">
+              <Link href={href}>{title}</Link>
+            </h2>
+            <p className="prose mb-2 text-gray-400 text-justify">{description}</p>
+            <div className="flex flex-wrap space-x-1.5 mb-3">
+              <span className="font-semibold text-gray-300">
+                {builtWith.join(', ')}
+              </span>
+            </div>
           </div>
-          <Link href={href} className="flextext-base font-medium text-primary hover:text-sky-400">
+          <Link href={href} className="font-medium text-primary hover:text-sky-400">
             {projectsConfig.detail} &rarr;
           </Link>
         </div>
--- data/projects.ts
+++ data/projects.ts
@@ -9,10 +9,17 @@ export type Project = {
 const projectsData: Project[] = [
   {
     title: 'Blog cá nhân',
-    description: `Trang web cá nhân của mình - nơi lưu trữ các bài viết về Toán học, Lập trình, các ý tưởng cá nhân, ...`,
+    description: 'Trang web cá nhân của mình - nơi lưu trữ các bài viết về Toán học, Lập trình, các ý tưởng cá nhân, ...',
     imgSrc: '/projects/3TBlog.png',
     builtWith: ['Typescript', 'Tailwind', 'Next.js'],
     href: '/',
+  },
+  {
+    title: 'Logic Puzzle Solver',
+    description: 'Mô hình hóa và giải các câu đố logic như Sudoku, Binox, Triox, Slitherlink, ...',
+    imgSrc: '/projects/LogicPuzzle.jpg',
+    builtWith: ['Python', 'MILP'],
+    href: 'https://github.com/Tung-hehe/LogicPuzzlesSolver',
   }
 ]
 
