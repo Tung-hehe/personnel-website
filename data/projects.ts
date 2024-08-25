95c68514ab8e6e4a9bb895fc02202919a686d671
Tran Thanh Tung <thanhtung1052001@gmail.com>
2024-08-26 00:02:47 +0700

    Add more project

--- data/projects.ts
+++ data/projects.ts
@@ -20,7 +20,21 @@ const projectsData: Project[] = [
     imgSrc: '/projects/LogicPuzzle.jpg',
     builtWith: ['Python', 'MILP'],
     href: 'https://github.com/Tung-hehe/LogicPuzzlesSolver',
-  }
+  },
+  {
+    title: 'MIM Python',
+    description: 'Trang web dạy lập trình python cho sinh viên khoa Toán - Cơ - Tin trường Đại học Khoa học Tự nhiên',
+    imgSrc: '/projects/MIMPython.png',
+    builtWith: ['Javascript', 'HTML', 'CSS'],
+    href: 'https://mimpython.github.io/',
+  },
+  {
+    title: 'Pixel Adventure Python',
+    description: 'Game 2D plartform lập trình bằng python và pygame, đang trong quá trình phát triển.',
+    imgSrc: '/projects/PixelAdventurePygame.png',
+    builtWith: ['Python', 'Pygame'],
+    href: 'https://github.com/Tung-hehe/Pixel-Adventure-Pygame',
+  },
 ]
 
 export default projectsData
