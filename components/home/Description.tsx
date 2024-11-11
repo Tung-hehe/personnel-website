b8a7bf2538fc14f9b9fe7fd0be833d619164c3c6
Tung-hehe <thanhtung1052001@gmail.com>
2024-11-11 14:15:33 +0700

    Fix prose

--- components/home/Description.tsx
+++ components/home/Description.tsx
@@ -3,10 +3,10 @@ import { homeConfig, LocaleType } from "@/data/config"
 export function Description({ locale }: { locale: LocaleType }) {
   return (
     <div className="mb-7 mt-3">
-      <div className="hidden sm:block">
+      <div className="hidden sm:block tracking-wide">
         {homeConfig.description[locale].map((text, i) => <p key={i}>{text}</p>)}
       </div>
-      <div className="sm:hidden">
+      <div className="sm:hidden break-words hyphens-auto text-justify tracking-wide">
         <p>{homeConfig.description[locale].join(' ')}</p>
       </div>
     </div>
