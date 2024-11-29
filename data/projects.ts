62c52c2a14857ef37414ac5db7bc5b26813c3f23
Tung-hehe <thanhtung1052001@gmail.com>
2024-11-29 16:01:12 +0700

    Add slitherling blog

--- data/projects.ts
+++ data/projects.ts
@@ -15,7 +15,7 @@ const projectsData: Project[] = [
       vi: 'Trang web cá nhân của mình - nơi lưu trữ các bài viết về toán học, lập trình và các ý tưởng cá nhân.',
       en: 'My personal website where I store articles about mathematics, programming and personal ideas'
     },
-    imgSrc: '/projects/TungTT.png',
+    imgSrc: '/projects/tungtt.png',
     builtWith: ['Typescript', 'Tailwind', 'Next.js'],
     href: '/',
   },
@@ -25,7 +25,7 @@ const projectsData: Project[] = [
       vi: 'Mô hình hóa và giải các câu đố logic như Sudoku, Binox, Triox, Slitherlink, ...',
       en: 'Modeling and solving logic puzzles such as Sudoku, Binox, Triox, Slitherlink, ...'
     },
-    imgSrc: '/projects/LogicPuzzle.jpg',
+    imgSrc: '/projects/logic_puzzle_solver.jpg',
     builtWith: ['Python', 'MILP'],
     href: 'https://github.com/.',
   },
@@ -35,7 +35,7 @@ const projectsData: Project[] = [
       vi: 'Trang web dạy lập trình python cho sinh viên khoa Toán - Cơ - Tin trường Đại học Khoa học Tự nhiên',
       en: 'A Python programming website for students in the faculty of Mathematics, Mechanics, and Informatics of HUS - VNU'
     },
-    imgSrc: '/projects/MIMPython.png',
+    imgSrc: '/projects/MIM_python.png',
     builtWith: ['Javascript', 'HTML', 'CSS'],
     href: 'https://mimpython.github.io/',
   },
@@ -45,7 +45,7 @@ const projectsData: Project[] = [
       vi: 'Game 2D plartform lập trình bằng python và pygame, đang trong quá trình phát triển.',
       en: 'A 2D platform game programmed in Python and Pygame, currently under development.',
     },
-    imgSrc: '/projects/PixelAdventurePygame.png',
+    imgSrc: '/projects/pixel_adventure_pygame.png',
     builtWith: ['Python', 'Pygame'],
     href: 'https://github.com/Tung-hehe/Pixel-Adventure-Pygame',
   },
